dce25fe84f86d3c6704427da92d2140692663d4c
Petter Bjørkås <pbjorkaas@enua.no>
2025-06-13 07:26:23 +0200

    Generate public folder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+def copy_to_public():
+    destination = "public"
+    src_dir = "static"
+    public_exists = os.path.exists(destination)
+    if public_exists:
+        shutil.rmtree(destination)
+    
+    shutil.copytree(src_dir, destination)
 
 def main():
+    copy_to_public()
     textNode = TextNode("Random text", TextType.Link, "https://cool.dev")
     print(textNode)
 
