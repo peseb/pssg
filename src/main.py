054ba6d93a51adc118945356d38e88e9a586eee0
Petter Bjørkås <pbjorkaas@enua.no>
2025-06-13 07:59:18 +0200

    WIP

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
 import os
 import shutil
 from textnode import TextNode, TextType
+from util.extract_title import extract_title
+from util.markdown_to_html_node import markdown_to_html_node
+
+def generate_page(from_path: str, template_path: str, dest_path: str):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    f = open(from_path)
+    markdown = f.read()
+    f.close()
+    f = open(template_path)
+    template = f.read()
+    f.close()
+
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dirname = os.path.dirname(dest_path)
+    os.makedirs(dirname)
+    
+
+
 
 def copy_to_public():
     destination = "public"
