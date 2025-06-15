3e4054d2cacf38980849ea4dbabee413e72d54e3
Petter Bjørkås <pbjorkaas@enua.no>
2025-06-15 09:35:09 +0200

    Add files.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,21 @@ import shutil
 from util.extract_title import extract_title
 from util.markdown_to_html_node import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str):
+    if os.path.isfile(dir_path_content):
+        path = os.path.join(dest_dir_path.replace(".md", ".html"))
+        print("Generated file at: ", path)
+        generate_page(dir_path_content, template_path, path)
+    else:
+        dir = os.listdir(dir_path_content)
+        for entry in dir:
+            print("Files: ", entry)
+            content_path = os.path.join(dir_path_content, entry)
+            dest_path = os.path.join(dest_dir_path, entry)
+            generate_pages_recursive(content_path, template_path, dest_path)
+            
+
+
 def generate_page(from_path: str, template_path: str, dest_path: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     f = open(from_path)
@@ -38,6 +53,6 @@ def copy_to_public():
 
 def main():
     copy_to_public()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 main()
